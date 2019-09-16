93aea304d48fbc83e8fdb653793b450bab2e1648
Vira <vira.romanko99@gmail.com>
2019-09-16 19:51:40 -0400

    Adding Objects and npm files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-import Person from "./modules/DataObject.js"
+import People from "./modules/DataObject.js"
+// import{ Team, People}
 
 // IIFE - Immediately Invoced Function Expression
 
@@ -6,7 +7,43 @@ import Person from "./modules/DataObject.js"
 
 console.log("fired!");
 
-console.log (Person);
+console.log (People);
+// grab the bio container and get ready to populate its content
+const bioInfo = document.querySelector(".bio-wrapper").children;
+     
+
+
+     function  showProfData  () {
+         //change the data on a page
+         // this is the label on the button (the name)
+         let currentProf = this.textContent;
+
+        // this refers to the heading tag (the first child of the bio-wrapper div)
+          bioInfo[0].textContent = People[currentProf].name;
+        // this refers to the paragraph tag ( the second child)
+        bioInfo[1].textContent = People[currentProf].role;
+        bioInfo[2].textContent = People[currentProf].bio;
+        
+         bioInfo[3].src = `images/${People[currentProf].image}`;
+         //bioInfo[2].src = "images/" + Person.image;
+         debugger;
+     }
+
+     for (let prof in People){
+         console.log(prof);
+
+         //create a button for every prof (every entry) in our object
+         let teamButton = document.createElement("button");
+         // set the buttons label(text) to the prof name (the cuttent entry in the object)
+
+         teamButton.textContent = prof;
+         // add some event handling so that when we click one of our new buttons we can show the right data
+         teamButton.addEventListener("click", showProfData);
+
+         //add the button to the section on our web page
+         document.querySelector('#section1').appendChild(teamButton);
+
+     }
 
 debugger;
 
--- js/modules/DataObject.js
+++ js/modules/DataObject.js
@@ -2,12 +2,51 @@
 // remember to separate the keys and values with a comma
 //and that's about it
 //and use a colon between the key and the value
+//an asosiative array
 
-const Person = {
+const Team = {
+    Trevor:{
+    bio:"Im stuck here let me go home",
     name: "Trevor",
     eyeColor: "blue",
     hairColor: "red",
-    role: "Professor"
+    role: "Professor",
+    image: "olaf.jpeg"
+    
+
+    },
+    Joe:{
+    bio:"I love hunny",
+    name: "Joe",
+    eyeColor: "broun",
+    hairColor: "reyish, more than me",
+    role: "Professor",
+    image: "pooh.jpeg"
+    
+    },
+
+    Jarrod: {
+    bio:" I have more than one look",
+    name: "Jarrod",
+    eyeColor: "blue",
+    hairColor: "blonde",
+    role: "Professor",
+    image: "zoolander.jpg"
+    
+    },
+
+    Justin:{
+    bio:"Help me out here",
+    name: "Jastin",
+    eyeColor: "brown",
+    hairColor: "black",
+    role: "Professor",
+    image: "justin.jpeg"
+    
+    }
+    
+    
 }
 
-export default Person;
+export default Team;
+//export {Team, People}
